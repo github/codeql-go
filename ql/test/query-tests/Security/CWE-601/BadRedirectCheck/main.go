88779a714a3886e1501fabab63702dc5ac42eaf7
Sauyon Lee <sauyon@github.com>
2020-04-27 04:12:51 -0700

    BadRedirectCheck: Add tests for FP checks

--- ql/test/query-tests/Security/CWE-601/BadRedirectCheck/main.go
+++ ql/test/query-tests/Security/CWE-601/BadRedirectCheck/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"net/url"
+	"path"
 	"strings"
 )
 
@@ -11,6 +13,10 @@ func badRedirect(redirect string, rw http.ResponseWriter, req *http.Request) {
 
 func goodRedirect(redirect string, rw http.ResponseWriter, req *http.Request) {
 	http.Redirect(rw, req, sanitizeUrlGood(redirect), 302)
+}
+
+func goodRedirect2(url string, rw http.ResponseWriter, req *http.Request) {
+	http.Redirect(rw, req, path.Join("/", sanitizeUrl(url)), 302)
 }
 
 func isValidRedir(redirect string) bool {
@@ -39,8 +45,48 @@ func isValidRedir1(redirect string) bool {
 	}
 }
 
-func goodRedirect2(url string, rw http.ResponseWriter, req *http.Request) {
+func goodRedirect3(url string, rw http.ResponseWriter, req *http.Request) {
 	if isValidRedirectGood(url) {
 		http.Redirect(rw, req, url, 302)
 	}
+}
+
+func getTarget(redirect string) string {
+	u, _ := url.Parse(redirect)
+
+	if u.Path[0] != "/" {
+		return "/"
+	}
+
+	return path.Clean(u.Path)
+}
+
+func goodRedirect4(url string, rw http.ResponseWriter, req *http.Request) {
+	http.Redirect(rw, req, getTarget(url), 302)
+}
+
+func getTarget1(redirect string) string {
+	if redirect[0] != "/" {
+		return "/"
+	}
+
+	return path.Clean(redirect)
+}
+
+func badRedirect2(url string, rw http.ResponseWriter, req *http.Request) {
+	http.Redirect(rw, req, getTarget1(url), 302)
+}
+
+func getTarget2(redirect string) string {
+	u, _ := url.Parse(redirect)
+
+	if u.Path[0] != "/" {
+		return "/"
+	}
+
+	return u.Path
+}
+
+func badRedirect2(url string, rw http.ResponseWriter, req *http.Request) {
+	http.Redirect(rw, req, getTarget2(url), 302)
 }
