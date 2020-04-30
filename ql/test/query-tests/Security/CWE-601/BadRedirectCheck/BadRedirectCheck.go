c1191ec3026e9239c715ea9b52b1a4f89775fe65
Sauyon Lee <sauyon@github.com>
2020-04-30 13:41:12 -0700

    BadRedirectCheck: remove unused imports from tests

--- ql/test/query-tests/Security/CWE-601/BadRedirectCheck/BadRedirectCheck.go
+++ ql/test/query-tests/Security/CWE-601/BadRedirectCheck/BadRedirectCheck.go
@@ -1,7 +1,5 @@
 package main
 
-import "net/http"
-
 func sanitizeUrl(redir string) string {
 	if len(redir) > 0 && redir[0] == '/' {
 		return redir
--- ql/test/query-tests/Security/CWE-601/BadRedirectCheck/BadRedirectCheckGood.go
+++ ql/test/query-tests/Security/CWE-601/BadRedirectCheck/BadRedirectCheckGood.go
@@ -1,7 +1,5 @@
 package main
 
-import "net/http"
-
 func sanitizeUrlGood(redir string) string {
 	if len(redir) > 1 && redir[0] == '/' && redir[1] != '/' && redir[1] != '\\' {
 		return redir
