ee4747a1933b06a90307c2d37555c5a4944e5d7f
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2020-06-30 10:12:02 +0000

    Update examples in help file
    Show both insecure examples and a secure example.

--- ql/src/experimental/CWE-322/InsecureIgnoreHostKeyExample.go
+++
@@ -1,8 +0,0 @@
-confg := &ssh.ClientConfig{
-	User: username,
-	Auth: []ssh.AuthMethod{nil},
-	HostKeyCallback: ssh.HostKeyCallback(
-		func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-			 return nil
-	}),,
-}
