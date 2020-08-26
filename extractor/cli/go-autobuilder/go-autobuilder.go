a6a85883edb0e86b0f538a8e2e6885e1fbc80eb3
Chris Smowton <smowton@github.com>
2020-08-26 12:21:03 +0100

    Don't try to use -mod=... when go.mod doesn't exist
    Also don't pass a blank argument to `go` when using an old version.

--- extractor/cli/go-autobuilder/go-autobuilder.go
+++ extractor/cli/go-autobuilder/go-autobuilder.go
@@ -509,7 +509,12 @@ func main() {
 
 	var cmd *exec.Cmd
 	log.Printf("Running extractor command '%s %s ./...' from directory '%s'.\n", extractor, modMode, cwd)
-	cmd = exec.Command(extractor, modMode.String(), "./...")
+	if depMode == GoGetWithModules && modMode.String() != "" {
+		cmd = exec.Command(extractor, modMode.String(), "./...")
+	} else {
+		cmd = exec.Command(extractor, "./...")
+	}
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
